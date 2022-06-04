48c6dcf69dc7a1fa9b9d06db9b10ffc53ccb972f
kevin-alv2208 <kevin.alv2208@gmail.com>
2022-06-04 11:47:45 -0600

    Creación de la Tienda

---
+++ src/test/java/com/komorebi/KomorebiDesingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.komorebi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KomorebiDesingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
